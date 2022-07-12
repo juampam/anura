2128020c0a78ecebc8ac8cc8dc76a9f03ddcdfb2
juampam <jpablomuralles@yandex.com>
2022-07-12 13:16:09 -0600

    cleaning code

---
+++ program/commands/content.py
@@ -0,0 +1,10 @@
+
+def example():
+    div = "\n\t\t<li><a href=\"#home\">Home</a></li>\n\t\t\t<li><a href=\"#news\">News</a></li>\n\t\t\t<li><a href=\"#contact\">Contact</a></li>\n\t\t\t<li><a href=\"#about\">About</a></li>"
+    nav = "\n\t<ul class=\"navbar\">" + div + "\n</ul> "
+    home = "<h1>Let's Jumping and Singing</h1>" + "\n<h3>Frogs are here!</h3>"
+    exp = nav + home
+    return exp
+def head():
+    res = str("\n\t<link rel=\"stylesheet\" href=\"style.css\">\n\t<link rel = \'icon\' href=\"../logo.svg\" type = \"image/x-icon\"/>")
+    return res 
--- program/commands/create.py
+++ program/commands/create.py
@@ -2,19 +2,16 @@
 import subprocess
 import os
 import shutil
-title = "Ganzo"
-def example():
-    div = "\n\t\t<li><a href=\"#home\">Home</a></li>\n\t\t\t<li><a href=\"#news\">News</a></li>\n\t\t\t<li><a href=\"#contact\">Contact</a></li>\n\t\t\t<li><a href=\"#about\">About</a></li>"
-    nav = "\n\t<ul class=\"navbar\">" + div + "\n</ul> " 
-    home = "<h1>Let's Jumping and Singing</h1>" + "\n<h3>Frogs are here!</h3>"
-    exp = nav + home
-    return exp
+import content
 
+title = "Hola"
 name = str("index.html")
-bcx = "Ganzo"
+bcx = "Hola"
 stylevar = ""
+
 if stylevar == "":
 	stylevar = "example.css"
+
 sFile = "styles/"+ stylevar
 os.mkdir(bcx)
 tFile = bcx + '/'+ 'style.css'
@@ -25,11 +22,11 @@ fileHandle = open(tFile, "w")
 for s in texts:
     fileHandle.write(s)
 fileHandle.close()
-print("stylevar"+" style added")
+print(stylevar +" style added")
 
 path = bcx +'/'+ name
-bcontent = example()
-hcontent = str("\t<title>"+title+"</title>\n\t<link rel=\"stylesheet\" href=\"style.css\">\n\t<link rel = \'icon\' href=\"../logo.svg\" type = \"image/x-icon\"/>")
+bcontent = content.example()
+hcontent = str("\t<title>"+title+"</title>") + content.head()
 head = str("<head>\n" + hcontent + "\n</head>\n")
 #componet = example()
 body = str("<body>\n"+bcontent+"\n</body>")
