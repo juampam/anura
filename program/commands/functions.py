8a8007e0e41ba266bc86d9852b25f9b1abf9f99f
juampam <jpablomuralles@yandex.com>
2022-07-13 12:55:45 -0600

    configuring outputs

--- program/commands/functions.py
+++ program/commands/functions.py
@@ -7,36 +7,55 @@ import index
 
 
 def create():
-    stylevar = index.getStyle()
-    title = str(index.getTitle())
-    name = str("index.html")
-    bcx = title
-    stylevar = ""
+    try:
+        stylevar = index.getStyle()
+        title = str(index.getTitle())
+        name = str("index.html")
+        bcx = title
+        
 
-    if stylevar == "":
-        stylevar = "example.css"
+        if stylevar == "":
+            stylevar = "example.css"
 
-    sFile = "styles/"+ stylevar
-    os.mkdir(bcx)
-    tFile = bcx + '/'+ 'style.css'
-    fileHandle = open(sFile, "r")
-    texts = fileHandle.readlines()
-    fileHandle.close()
-    fileHandle = open(tFile, "w")
-    for s in texts:
-        fileHandle.write(s)
-    fileHandle.close()
-    print(stylevar +" style added")
+        sFile = "styles/"+ stylevar
+        os.mkdir(bcx)
+        tFile = bcx + '/'+ 'style.css'
+        fileHandle = open(sFile, "r")
+        texts = fileHandle.readlines()
+        fileHandle.close()
+        fileHandle = open(tFile, "w")
+        
+        for s in texts:
+            fileHandle.write(s)
+        
+        fileHandle.close()
+        print("style.css added using: "+ stylevar )
 
-    path = bcx +'/'+ name
-    bcontent = content.example()
-    hcontent = content.head()
-    head = str("<head>\n" + hcontent + "\n</head>\n")
-    #componet = example()
-    body = str("<body>\n"+bcontent+"\n</body>")
-    file = open(path, "w")
-    file.write("<!DOCTYPE HTML>" + os.linesep)
-    file.write(head)
-    file.write(body)
-    file.close()
+    except Exception as e:
+        errsty = str("[Errno 2] No such file or directory: " + "\'"+ sFile + "\'")
+        if str(e) == errsty:
+            print("Anura Error: "+ stylevar + " does not exist")
+            shutil.rmtree(bcx)   
+
+    try:
+        
+        path = bcx +'/'+ name
+        bcontent = content.example()
+        hcontent = content.head()
+        head = str("<head>\n" + hcontent + "\n</head>\n")
+        #componet = example()
+        body = str("<body>\n"+bcontent+"\n</body>")
+        file = open(path, "w")
+        file.write("<!DOCTYPE HTML>" + os.linesep)
+        file.write(head)
+        file.write(body)
+        file.close()
+
+    except Exception as e:
+        errcon = str("[Errno 17] File exists: "+ "\'"+ title + "\'")
+        errsty = str("[Errno 2] No such file or directory: " + "\'"+ sFile + "\'")
+        if str(e) == errcon:
+            print("Anura Error: "+ title + " alredy exists")
+        elif str(e) == errsty:
+            print("Anura Error: "+ sFile + " does not exist")
 create()
